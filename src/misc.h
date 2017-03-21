89b1070dbe351e6cc90dde51fd4c1ba03ed9bbcb
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-03-21 18:34:26 +0100

    Fix build on windows

--- src/misc.h
+++ src/misc.h
@@ -25,6 +25,6 @@ string_format (const std::string &format, Args ...args)
 {
     size_t size = snprintf (nullptr, 0, format.c_str (), args...) + 1;
     std::unique_ptr<char[]> buf (new char[size]);
-    std::snprintf (buf.get(), size, format.c_str (), args...);
+    snprintf (buf.get(), size, format.c_str (), args...);
     return std::string (buf.get (), buf.get () + size - 1);
 }
