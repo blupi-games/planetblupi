57b6190a5368740a0e9171f78ab2d82d8156480c
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-08-03 22:51:40 +0200

    Add license header in all source files

--- src/path.cxx
+++ src/path.cxx
@@ -1,5 +1,22 @@
-
-// (c) 1997, Denis Dumoulin
+/*
+ * This file is part of the planetblupi source code
+ * Copyright (C) 1997 Daniel Roux, EPSITEC SA & Denis Dumoulin
+ * Copyright (C) 2017 Mathieu Schroeter
+ * http://epsitec.ch; http://www.blupi.org; http://github.com/blupi-games
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://gnu.org/licenses
+ */
 
 #include "decor.h"
 #include "fifo.h"
