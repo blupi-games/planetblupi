a80c3d09fcf741d666925b470379e2d15f10b7ec
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-08-13 17:03:23 +0200

    Cosmetic

--- src/blupi.cxx
+++ src/blupi.cxx
@@ -87,9 +87,7 @@ static void split (const std::string & s, char delim, Out result)
   ss.str (s);
   std::string item;
   while (std::getline (ss, item, delim))
-  {
     *(result++) = item;
-  }
 }
 
 static std::vector<std::string> split (const std::string & s, char delim)
