97482dcbbbf485a5cbf95dab1c28bb6bf8f4f030
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-08-13 16:52:30 +0200

    Add missing static specifier

--- src/blupi.cxx
+++ src/blupi.cxx
@@ -81,7 +81,7 @@ struct url_data {
 };
 
 template <typename Out>
-void split (const std::string & s, char delim, Out result)
+static void split (const std::string & s, char delim, Out result)
 {
   std::stringstream ss;
   ss.str (s);
@@ -92,7 +92,7 @@ void split (const std::string & s, char delim, Out result)
   }
 }
 
-std::vector<std::string> split (const std::string & s, char delim)
+static std::vector<std::string> split (const std::string & s, char delim)
 {
   std::vector<std::string> elems;
   split (s, delim, std::back_inserter (elems));
