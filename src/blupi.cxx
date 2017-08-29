a1549544d49fdbf4b118afe0031a882ff65ac212
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-08-29 09:10:32 +0200

    Fix test against getline return value

--- src/blupi.cxx
+++ src/blupi.cxx
@@ -92,7 +92,7 @@ split (const std::string & s, char delim, Out result)
   std::stringstream ss;
   ss.str (s);
   std::string item;
-  while (std::getline (ss, item, delim))
+  while (std::getline (ss, item, delim).good ())
     *(result++) = item;
 }
 
