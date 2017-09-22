blupi-games/planetblupi

commit a1549544d49fdbf4b118afe0031a882ff65ac212

Fix test against getline return value

src/blupi.cxx
@@ -92,7 +92,7 @@ split (const std::string & s, char delim, Out result)
   std::stringstream ss;
   ss.str (s);
   std::string item;
-  while (std::getline (ss, item, delim))
+  while (std::getline (ss, item, delim).good ())
     *(result++) = item;
 }
 

commit 1dcddd1f6c4bb138f4f66012656cdf71c39d5633

Fix regression introduced by a1549544d49fdbf4b118afe0031a882ff65ac212
The next version must be 1.10 instead of 1.9.3 in order to re-enable the
check update on all v1.9 versions.

src/blupi.cxx
@@ -94,7 +94,7 @@ split (const std::string & s, char delim, Out result)
   std::stringstream ss;
   ss.str (s);
   std::string item;
-  while (std::getline (ss, item, delim).good ())
+  while (std::getline (ss, item, delim))
     *(result++) = item;
 }
 