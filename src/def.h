b9ab6272f8e67613e632e1d6078b79a3cabb8fee
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-10-04 07:06:12 +0200

    Update comments to english

--- src/def.h
+++ src/def.h
@@ -23,50 +23,50 @@
 #include <SDL2/SDL_stdinc.h>
 
 // clang-format off
-#define _INTRO          true    // true si images d'introduction
+#define _INTRO          true    // true for init screen
 
 #define SCRFACTOR       4 / 3
-#define LXIMAGE         (480 * SCRFACTOR + (480 * SCRFACTOR) % 2)     // dimensions de la fenêtre de jeu
+#define LXIMAGE         (480 * SCRFACTOR + (480 * SCRFACTOR) % 2) // window size
 #define LYIMAGE         480
 
-#define POSDRAWX        144     // surface de dessin
+#define POSDRAWX        144     // draw surface
 #define POSDRAWY        15
 #define DIMDRAWX        (LXIMAGE - (640 - 480))
 #define DIMDRAWY        450
 
-#define POSMAPX         8       // surface pour la carte
+#define POSMAPX         8       // map surface
 #define POSMAPY         15
 #define DIMMAPX         128
 #define DIMMAPY         128
 
-#define MAXCELX         200     // nb max de cellules d'un monde
+#define MAXCELX         200     // max cells for a world
 #define MAXCELY         200
 
-#define DIMCELX         60      // dimensions d'une cellule (décor)
+#define DIMCELX         60      // cell size (decor)
 #define DIMCELY         30
 
-#define DIMOBJX         120     // dimensions d'un objet
+#define DIMOBJX         120     // object size
 #define DIMOBJY         120
 
-#define DIMBLUPIX       60      // dimensions de blupi
+#define DIMBLUPIX       60      // Blupi size
 #define DIMBLUPIY       60
-#define SHIFTBLUPIY     5       // petit décalage vers le haut
+#define SHIFTBLUPIY     5       // shift on top
 
-#define DIMBUTTONX      40      // dimensions d'un button
+#define DIMBUTTONX      40      // button size
 #define DIMBUTTONY      40
 
-#define DIMJAUGEX       124     // dimensions de la jauge
+#define DIMJAUGEX       124     // progress size
 #define DIMJAUGEY       22
 
-#define POSSTATX        12      // statistiques
+#define POSSTATX        12      // statistics
 #define POSSTATY        220
 #define DIMSTATX        60
 #define DIMSTATY        30
 
-#define DIMTEXTX        16      // dimensions max d'un caractère
+#define DIMTEXTX        16      // max char size
 #define DIMTEXTY        16
 
-#define DIMLITTLEX      16      // dimensions max d'un petit caractère
+#define DIMLITTLEX      16      // max small char size
 #define DIMLITTLEY      12
 
 #define CHBACK          0
