e9f7a71b9b186c3eb29bf45adc7f3733bceb126d
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-12-01 13:57:34 +0100

    Add a stupid bootstrap script for the NSIS template

---
+++ resources/nsis/bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp $(dirname $0)/NSIS.template.in /C/msys64/mingw64/share/cmake-3.9/Modules/NSIS.template.in
+cp $(dirname $0)/NSIS.template.in /C/msys64/mingw32/share/cmake-3.9/Modules/NSIS.template.in
