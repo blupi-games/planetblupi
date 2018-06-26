93f5389132affcbb7340ef181ff8fe5c244cb9ef
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-06-26 19:14:55 +0200

    Cosmetics

--- src/pixmap.cxx
+++ src/pixmap.cxx
@@ -914,19 +914,20 @@ CPixmap::LoadCursors ()
 {
   Uint32 rmask, gmask, bmask, amask;
 
-/* SDL interprets each pixel as a 32-bit number, so our masks must depend
-on the endianness (byte order) of the machine */
+  /* SDL interprets each pixel as a 32-bit number, so our masks must depend
+   * on the endianness (byte order) of the machine
+   */
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
   rmask = 0xff000000;
   gmask = 0x00ff0000;
   bmask = 0x0000ff00;
   amask = 0x000000ff;
-#else
+#else /* SDL_BYTEORDER == SDL_BIG_ENDIAN */
   rmask = 0x000000ff;
   gmask = 0x0000ff00;
   bmask = 0x00ff0000;
   amask = 0xff000000;
-#endif
+#endif /* SDL_BYTEORDER != SDL_BIG_ENDIAN */
 
   auto scale = this->GetDisplayScale ();
 
