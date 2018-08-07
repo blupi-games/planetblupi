4a18b2e9ea5cf9fb7f2ec9131a7b85a4ed21eadd
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-08-07 16:42:04 +0200

    Replace unavailable macro by usual message macro

--- cmake/Ronn2Man.cmake
+++ cmake/Ronn2Man.cmake
@@ -15,10 +15,10 @@ FIND_PROGRAM(GZIP gzip)
 
 IF (NOT RONN OR NOT GZIP)
   IF (NOT RONN)
-    BUILD_WARNING ("ronn not found, manpages won't be generated")
+    message(WARNING "ronn not found, manpages won't be generated")
   ENDIF(NOT RONN)
   IF (NOT GZIP)
-    BUILD_WARNING ("gzip not found, manpages won't be generated")
+    message(WARNING "gzip not found, manpages won't be generated")
   ENDIF(NOT GZIP)
   # empty macro
   MACRO(manpage MANFILE)
