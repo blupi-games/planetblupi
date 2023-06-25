da13271d5e76a4a53c1b4e1457d0aa21fab31704
Mathieu Schroeter <schroeter@epsitec.ch>
2023-06-25 18:14:38 +0200

    Sync CPack NSIS with latest CMake

--- resources/nsis/bootstrap.sh
+++ resources/nsis/bootstrap.sh
@@ -2,7 +2,7 @@
 
 CMAKE_VERSION=3.15
 
-mv /C/msys64/mingw64/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in /C/msys64/mingw64/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in.orig
-mv /C/msys64/mingw32/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in /C/msys64/mingw32/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in.orig
-cp $(dirname $0)/NSIS.template.in /C/msys64/mingw64/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in
-cp $(dirname $0)/NSIS.template.in /C/msys64/mingw32/share/cmake-$CMAKE_VERSION/Modules/NSIS.template.in
+mv /C/msys64/mingw64/share/cmake/Modules/Internal/CPack/NSIS.template.in /C/msys64/mingw64/share/cmake/Modules/Internal/CPack/NSIS.template.in.orig
+mv /C/msys64/mingw32/share/cmake/Modules/Internal/CPack/NSIS.template.in /C/msys64/mingw32/share/cmake/Modules/Internal/CPack/NSIS.template.in.orig
+cp $(dirname $0)/NSIS.template.in /C/msys64/mingw64/share/cmake/Modules/Internal/CPack/NSIS.template.in
+cp $(dirname $0)/NSIS.template.in /C/msys64/mingw32/share/cmake/Modules/Internal/CPack/NSIS.template.in
